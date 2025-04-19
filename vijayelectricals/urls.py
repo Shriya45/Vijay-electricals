326b8cc34ca5f860633b2d8fcae7fafdb426245b
Shriya Nimje <148790900+Shriya45@users.noreply.github.com>
2025-04-19 16:50:02 +0530

    Added sitemap support

--- vijayelectricals/urls.py
+++ vijayelectricals/urls.py
@@ -6,6 +6,8 @@ from app.views import index, chatbot_response
 from django.contrib.sitemaps.views import sitemap
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
+from app.sitemap import StaticViewSitemap  # make sure this is correct
+from django.http import HttpResponse
 
 class StaticViewSitemap(Sitemap):
     def items(self):
@@ -13,6 +15,10 @@ class StaticViewSitemap(Sitemap):
 
     def location(self, item):
         return reverse(item)
+def custom_sitemap(request):
+    response = sitemap(request, sitemaps=sitemaps)
+    response['X-Robots-Tag'] = 'index, follow'
+    return response
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -24,6 +30,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
      path("chatbot/", chatbot_response, name="chatbot"),
     path('', include('app.urls')),  # include our app's urls
+    path('sitemap.xml', custom_sitemap, name='sitemap'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),  # sitemap path
 ]
 
